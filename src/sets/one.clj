0d573a8a96ee37699cfd50174dc74c0ad02a63db
Orlando William <orlando.cu@hotmail.com>
2019-09-16 18:34:06 -0400

    6 - hamming

--- src/sets/one.clj
+++ src/sets/one.clj
@@ -93,3 +93,32 @@ I go crazy when I hear a cymbal"
        (Hex/encodeHex)
        (String.)))
 ;; => "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+
+;; #6
+;; -------------------------
+
+(defn bit-count* [b]
+  (loop [b b
+         count 0]
+    (if (zero? b)
+      count
+      (recur
+       (bit-shift-right b 1)
+       (+ count (bit-and 1 b))))))
+
+(def bit-counts
+  (->>
+   (range 128)
+   (map byte)
+   (map bit-count*)
+   vec))
+
+(defn hamming [bs1 bs2]
+  (apply + (map (comp bit-counts bit-xor) bs1 bs2)))
+
+(hamming (.getBytes "this is a test")
+         (.getBytes "wokka wokka!!!"))
+;; => 37
+
+(let [file6 (slurp "https://cryptopals.com/static/challenge-data/6.txt")])
