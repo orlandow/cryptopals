c75c7ae45699636a2237587f877870c9d1867c9e
Orlando William <orlando.cu@hotmail.com>
2019-09-17 19:29:32 -0400

    8 - solved

--- src/sets/one.clj
+++ src/sets/one.clj
@@ -184,6 +184,32 @@ I go crazy when I hear a cymbal"
 
 (defonce file8 (slurp "https://cryptopals.com/static/challenge-data/8.txt"))
 
-(def blocks7
+(def blocks8
   (->> (str/split-lines file8)
        (map #(Hex/decodeHex %))))
+
+(defn ecbd-prob [bs]
+  (->> (frequencies bs)
+       (filter #(> (second %) 1))
+       (map second)
+       (apply +)))
+
+(->> blocks8
+     (map #(partition 16 %))
+     (map-indexed (fn [i b] {:prob (ecbd-prob b)
+                             :block b
+                             :line (inc i)}))
+     (sort-by :prob)
+     last
+     :line)
+;; => 133
+
+
+(->> blocks8
+     (map-indexed (fn [i b] {:prob (ecbd-prob b)
+                             :block b
+                             :line (inc i)}))
+     (sort-by :prob)
+     last
+     :line)
+;; => 133
